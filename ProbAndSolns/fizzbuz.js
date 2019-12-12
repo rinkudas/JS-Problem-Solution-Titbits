cd8445e690a39f1db24bc7dc516a0ae60c60c463
Rinku Das <rinkudas.id@gmail.com>
2019-12-12 22:22:56 +0530

    FizzBuzz

---
+++ ProbAndSolns/fizzbuz.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @param {number} num 
+ * @param {number} mod
+ * @return {number}
+ */
+
+function isMultiple(num, mod){
+  return num % mod === 0;
+}
+
+/**
+ * 
+ * @param {number} num
+ * @return {number or string}
+ */
+
+function fizzBuzz(num){
+  switch(true){
+    case isMultiple(num, 15): return "FizzBuzz";
+    case isMultiple(num, 3): return "Fizz";
+    case isMultiple(num, 5): return "Buzz";
+    default: return num;
+  }
+}
